d0b300befe9d6a795e2e60b5c1c27b292ae51b1b
AdelinoJnr <adelinojunior96@gmail.com>
2021-11-02 10:08:18 -0300

    config: estabelecendo coneccao com mongodb

---
+++ server/src/database/index.ts
@@ -0,0 +1,4 @@
+// Simulando variaveis de ambiente
+export const MONGO_DB_URL: string = 'mongodb://localhost:27017/goflux';
+export const DB_NAME: string = 'goflux';
+export const PORT: number = 3333;
