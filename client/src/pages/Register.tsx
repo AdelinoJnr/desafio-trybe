e953169dd27601b2ba5808324e6af2f5cb144c32
AdelinoJnr <adelinojunior96@gmail.com>
2021-11-03 14:15:22 -0300

    feat: logica para criacao de usuario

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { api } from '../services/api';
+
+interface Payload {
+  name: string,
+  email: string,
+  password: string,
+}
 
 function Register(): JSX.Element {
   const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  const createUser = async (ev: any) => {
+    ev.preventDefault();
+    const payload: Payload = { name, email, password };
+    try {
+      const { data } = await api.post('api/users', payload);
+      alert(`Usuario ${data.name} foi cadastrado`);
+    } catch (_e) {
+      console.log('Deu ruim!');
+      alert('Não foi possivel Cadastrar');
+    }
+  };
 
   return (
     <>
@@ -30,7 +49,7 @@ function Register(): JSX.Element {
         onChange={(ev) => setPassword(ev.target.value)}
         placeholder="Digite sua senha"
       />
-      <button type="submit">Cadastrar</button>
+      <button type="submit" onClick={createUser} >Cadastrar</button>
       <Link to="/login">Ja tenho cadastro</Link>
     </>
   );
