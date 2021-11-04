848b281b0d52e4cccea1ba393509897ecff18b6d
AdelinoJnr <adelinojunior96@gmail.com>
2021-11-04 18:34:54 -0300

    refactor: @types centralizados

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -48,7 +48,7 @@ function Task({ task, length, setRenderPage, renderPage, setTasks }: Props): JSX
       await api.delete(`api/tasks/${_id}`);
       setRenderPage(renderPage + 1);
     } catch (_e) {
-      console.log('Deu ruim!');  
+      return; 
     }
   };
 
@@ -60,7 +60,7 @@ function Task({ task, length, setRenderPage, renderPage, setTasks }: Props): JSX
       setTasks(data);
       setRenderPage(renderPage + 1);
     } catch (_e) {
-      console.log('Deu ruim!');
+      return;
     }
     setUpdate(true);
   };
